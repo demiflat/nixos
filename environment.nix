24e6443237f1c31a0d40d4bba3584b51c22e04df
Darrell Kundel <darrell@demiflat.com>
2024-01-19 12:42:31 -0800

    cp

--- environment.nix
+++ environment.nix
@@ -16,14 +16,15 @@
   };
 
   # env
-  # Print the URL instead on servers
-  environment.sessionVariables = { 
 #  environment.variables = { 
-     BROWSER = "echo";
+  environment.sessionVariables = { 
+     QT_QPA_PLATFORMTHEME = "qt5ct";
+     # Print the URL instead on servers
+     #BROWSER = "echo";
      MOZ_ENABLE_WAYLAND = "1";
      MOZ_DBUS_REMOTE = "1";
      HSA_OVERRIDE_GFX_VERSION = "10.3.0";
-     DEVICE = "cuda";
+     #DEVICE = "cuda";
   };
 
 }
