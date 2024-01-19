0a43cff3ee42650d7b148ce8221921192f540d8a
Darrell Kundel <darrell@demiflat.com>
2024-01-18 21:04:46 -0800

    rocm

--- environment.nix
+++ environment.nix
@@ -8,6 +8,8 @@
   environment.sessionVariables = {
      MOZ_ENABLE_WAYLAND = "1";
      MOZ_DBUS_REMOTE = "1";
+     HSA_OVERRIDE_GFX_VERSION = 10.3.0;
+     DEVICE = cuda;
  #    XDG_CURRENT_DESKTOP = "sway"; 
   };
 }
