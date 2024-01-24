9e60cdc603df4f2576bdc9f236c70a07267077ff
Darrell Kundel <darrell@demiflat.com>
2024-01-23 21:55:06 -0800

    rocm

--- environment.nix
+++ environment.nix
@@ -23,8 +23,11 @@
      #BROWSER = "echo";
      MOZ_ENABLE_WAYLAND = "1";
      MOZ_DBUS_REMOTE = "1";
-     HSA_OVERRIDE_GFX_VERSION = "10.3.0";
-     DEVICE = "cuda";
+     #HSA_OVERRIDE_GFX_VERSION = "10.3.0";
+     HSA_OVERRIDE_GFX_VERSION = "9.0.0";
+     #DEVICE = "cuda";
+     PYTORCH_ROCM_ARCH = "gfx900";
+     USE_ROCM = "1";
   };
 
 }
