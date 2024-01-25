0a8391c553435d134413914b46865cc25fe1a22d
Darrell Kundel <darrell@demiflat.com>
2024-01-25 14:13:03 -0800

    env

--- environment.nix
+++ environment.nix
@@ -18,19 +18,37 @@
   # env
 #  environment.variables = { 
   environment.sessionVariables = { 
-     GTK_THEME = "Mojave-Dark";
-     QT_QPA_PLATFORMTHEME = "qt5ct";
-     # Print the URL instead on servers
-     #BROWSER = "echo";
-     MOZ_ENABLE_WAYLAND = "1";
-     MOZ_DBUS_REMOTE = "1";
-     #HSA_OVERRIDE_GFX_VERSION = "10.3.0";
-     HSA_OVERRIDE_GFX_VERSION = "9.0.0";
-     #DEVICE = "cuda";
-     #PYTORCH_ROCM_ARCH = "gfx90c";
-     PYTORCH_ROCM_ARCH = "gfx900";
-     #PYTORCH_ROCM_ARCH = "gfx1030";
-     USE_ROCM = "1";
+     
+    # Print the URL instead on servers
+    #BROWSER = "echo";
+    #HSA_OVERRIDE_GFX_VERSION = "10.3.0";
+    HSA_OVERRIDE_GFX_VERSION = "9.0.0";
+    #DEVICE = "cuda";
+    #PYTORCH_ROCM_ARCH = "gfx90c";
+    PYTORCH_ROCM_ARCH = "gfx900";
+    #PYTORCH_ROCM_ARCH = "gfx1030";
+    USE_ROCM = "1";
+
+    GTK_THEME = "Mojave-Dark";
+    XDG_SESSION_TYPE = "wayland";
+    XDG_SESSION_DESKTOP = "Hyprland";
+
+    GDK_BACKEND = "wayland";
+    QT_QPA_PLATFORM = "wayland";
+    QT_QPA_PLATFORMTHEME = "qt5ct";
+    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+
+    SDL_VIDEODRIVER = "wayland";
+    _JAVA_AWT_WM_NONREPARENTING = "1";
+    WLR_NO_HARDWARE_CURSORS = "1";
+    WLR_DRM_NO_ATOMIC = "1";
+
+    MOZ_DISABLE_RDD_SANDBOX = "1";
+    MOZ_ENABLE_WAYLAND = "1";
+    MOZ_DBUS_REMOTE = "1";
+
+    OZONE_PLATFORM = "wayland";
   };
 
 }
