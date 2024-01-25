4f0f433c1610f3f48c43684feec8b3ec3c08b17c
Darrell Kundel <darrell@demiflat.com>
2024-01-24 23:49:17 -0800

    gfx

--- environment.nix
+++ environment.nix
@@ -26,8 +26,9 @@
      #HSA_OVERRIDE_GFX_VERSION = "10.3.0";
      HSA_OVERRIDE_GFX_VERSION = "9.0.0";
      #DEVICE = "cuda";
-     #PYTORCH_ROCM_ARCH = "gfx900";
-     PYTORCH_ROCM_ARCH = "gfx1030";
+     #PYTORCH_ROCM_ARCH = "gfx90c";
+     PYTORCH_ROCM_ARCH = "gfx900";
+     #PYTORCH_ROCM_ARCH = "gfx1030";
      USE_ROCM = "1";
   };
 
