a6fb30e747909247d4dabdcf759d0a9591db4a9a
Darrell Kundel <darrell@demiflat.com>
2024-03-25 22:57:18 -0700

    initrd.systemd

--- hardware.nix
+++ hardware.nix
@@ -5,6 +5,7 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
+  boot.initrd.systemd.enable = true;
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ "kvm-amd" "amdgpu" "i2c-dev" ];
   boot.kernelModules = [ "kvm-amd" "amdgpu" "i2c-dev" ];
