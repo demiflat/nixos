18df720ea68e90c4102f4d40994124ed59112c4d
Darrell Kundel <darrell@demiflat.com>
2024-04-29 13:59:32 -0700

    -env

--- environment.nix
+++ environment.nix
@@ -30,18 +30,18 @@
     #PYTORCH_ROCM_ARCH = "gfx1030";
     USE_ROCM = "1";
 
-    GTK_THEME = "Mojave-Dark";
-    XDG_SESSION_TYPE = "wayland";
-    XDG_SESSION_DESKTOP = "Hyprland";
+    #GTK_THEME = "Mojave-Dark";
+    #XDG_SESSION_TYPE = "wayland";
+    #XDG_SESSION_DESKTOP = "Hyprland";
 
     #GDK_BACKEND = "wayland";
-    QT_QPA_PLATFORM = "wayland";
-    QT_QPA_PLATFORMTHEME = "qt5ct";
-    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
-    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+    #QT_QPA_PLATFORM = "wayland";
+    #QT_QPA_PLATFORMTHEME = "qt5ct";
+    #QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+    #QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
 
-    SDL_VIDEODRIVER = "wayland";
-    _JAVA_AWT_WM_NONREPARENTING = "1";
+    #SDL_VIDEODRIVER = "wayland";
+    #_JAVA_AWT_WM_NONREPARENTING = "1";
     # nvidia:
     #WLR_NO_HARDWARE_CURSORS = "1";
     #WLR_DRM_NO_ATOMIC = "1";
