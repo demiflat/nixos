8af97bfad0204867881eed4c31f3bd83a3935c8b
Darrell Kundel <darrell@demiflat.com>
2024-06-10 00:08:55 -0700

    programs refactor

--- programs.nix
+++ programs.nix
@@ -1,58 +1,59 @@
 { config, lib, modulesPath, pkgs, ... }:
 {
-  programs.bcc.enable = true;
-  programs.sysdig.enable = true;
-  programs.dconf.enable = true;
-  programs.direnv.enable = true;
-  programs.mtr.enable = true;
-  programs.wireshark.enable = true;
-  programs.iotop.enable = true;
-  programs.iftop.enable = true;
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-    viAlias = true;
-    vimAlias = true;
-#    plugins = [
-#      pkgs.vimPlugins.nvim-treesitter.withAllGrammars
-#    ];
-  };
-  programs.ssh.startAgent = true;
-  programs.gnupg.agent = {
-    enable = true;
-#    enableSSHSupport = true;
-    pinentryPackage = pkgs.pinentry-curses;
-  };
+  programs = {
+    bcc.enable = true;
+    sysdig.enable = true;
+    dconf.enable = true;
+    direnv.enable = true;
+    mtr.enable = true;
+    wireshark.enable = true;
+    iotop.enable = true;
+    iftop.enable = true;
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+      viAlias = true;
+      vimAlias = true;
+  #    plugins = [
+  #      pkgs.vimPlugins.nvim-treesitter.withAllGrammars
+  #    ];
+    };
+    ssh.startAgent = true;
+    gnupg.agent = {
+      enable = true;
+  #    enableSSHSupport = true;
+      pinentryPackage = pkgs.pinentry-curses;
+    };
 
-  programs.bash.vteIntegration = true;
-  
-  programs.chromium.enable = true;
-  programs.criu.enable = true;
-  programs.evince.enable = true;
-  programs.file-roller.enable = true;
-  programs.firefox.enable = true;
-  programs.git.enable = true;
-  programs.git.prompt.enable = true;
-  programs.htop.enable = true;
-  programs.mosh.enable = true;
+    bash.vteIntegration = true;
+    
+    chromium.enable = true;
+    criu.enable = true;
+    evince.enable = true;
+    file-roller.enable = true;
+    firefox.enable = true;
+    git.enable = true;
+    git.prompt.enable = true;
+    htop.enable = true;
+    mosh.enable = true;
 
-  programs.sway.enable = true;
-  programs.hyprland.enable = true;
-  programs.hyprland.xwayland.enable = true;
-  programs.waybar.enable = true;
-  security.polkit.enable = true;
-  programs.fish.enable = true;
+  #  sway.enable = true;
+    hyprland.enable = true;
+    hyprland.xwayland.enable = true;
+    waybar.enable = true;
+    security.polkit.enable = true;
+    fish.enable = true;
 
-  # Avoid TOFU MITM with github by providing their public key here.
-  programs.ssh.knownHosts = {
-    "github.com".hostNames = [ "github.com" ];
-    "github.com".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
+    # Avoid TOFU MITM with github by providing their public key here.
+    ssh.knownHosts = {
+      "github.com".hostNames = [ "github.com" ];
+      "github.com".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
 
-    "gitlab.com".hostNames = [ "gitlab.com" ];
-    "gitlab.com".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfuCHKVTjquxvt6CM6tdG4SLp1Btn/nOeHHE5UOzRdf";
+      "gitlab.com".hostNames = [ "gitlab.com" ];
+      "gitlab.com".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfuCHKVTjquxvt6CM6tdG4SLp1Btn/nOeHHE5UOzRdf";
 
-    "git.sr.ht".hostNames = [ "git.sr.ht" ];
-    "git.sr.ht".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMZvRd4EtM7R+IHVMWmDkVU3VLQTSwQDSAvW0t2Tkj60";
-  };
-        
+      "git.sr.ht".hostNames = [ "git.sr.ht" ];
+      "git.sr.ht".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMZvRd4EtM7R+IHVMWmDkVU3VLQTSwQDSAvW0t2Tkj60";
+    };
+  };      
 }
