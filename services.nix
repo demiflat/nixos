434313a8a9932e30279c74fb1240a66e57e624d0
Darrell Kundel <darrell@demiflat.com>
2024-07-15 16:00:16 -0700

    rocm

--- services.nix
+++ services.nix
@@ -1,11 +1,15 @@
-{ config, lib, modulesPath, ... }:
 {
+  config,
+  lib,
+  modulesPath,
+  ...
+}: {
   services.resolved = {
     enable = true;
     dnssec = "false";
     llmnr = "false";
     #domains = [ "demiflat.org" ];
-    fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+    fallbackDns = ["1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one"];
     #extraConfig = ''
     #  DNSOverTLS=yes
     #'';
@@ -32,14 +36,14 @@
     enable = true;
     xkb.layout = "us";
     xkb.variant = "";
-    videoDrivers = [ "amdgpu" ];
+    videoDrivers = ["amdgpu"];
     displayManager.gdm.enable = true;
     displayManager.gdm.wayland = true;
     desktopManager.gnome.enable = true;
-#    displayManager.sddm.enable = true;
-#    displayManager.sddm.wayland.enable = true;
-#    desktopManager.plasma5.enable = true;
-#    desktopManager.plasma6.enable = true;
+    #    displayManager.sddm.enable = true;
+    #    displayManager.sddm.wayland.enable = true;
+    #    desktopManager.plasma5.enable = true;
+    #    desktopManager.plasma6.enable = true;
   };
 
   # gnome-keyring for the secrets management service. Also adds its
@@ -49,7 +53,7 @@
 
   # Flatpak is useful to get a couple things that aren't packaged for
   # NixOS, like Parsec.
-  services.flatpak.enable = true;  
+  services.flatpak.enable = true;
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
@@ -64,7 +68,7 @@
   services.pipewire = {
     enable = true;
     alsa.enable = true;
-#    alsa.support32Bit = true;
+    #    alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
 
@@ -80,7 +84,7 @@
 
   # udev
   services.udev.extraRules = ''
-    # solokey  
+    # solokey
     SUBSYSTEMS=="usb", ATTRS{idVendor}=="1d6b", ATTRS{idProduct}=="0002", TAG+="uaccess", MODE="0666", SYMLINK+="solo"
     # cidoo
     SUBSYSTEMS=="usb", ATTRS{idVendor}=="1ea7", ATTRS{idProduct}=="7777", TAG+="uaccess", MODE="0666", SYMLINK+="cidoo"
@@ -91,8 +95,8 @@
   services.openssh.enable = true;
   services.openssh = {
     settings.X11Forwarding = false;
-  #  settings.KbdInteractiveAuthentication = false;
-  #  settings.PasswordAuthentication = false;
+    #  settings.KbdInteractiveAuthentication = false;
+    #  settings.PasswordAuthentication = false;
     settings.UseDns = false;
 
     # Use key exchange algorithms recommended by `nixpkgs#ssh-audit`
@@ -109,8 +113,9 @@
     # However those match blocks cannot be put after other `extraConfig` lines
     # with the current sshd config module, which is however something the sshd
     # config parser mandates.
-    authorizedKeysFiles = lib.mkIf (!config.services.gitea.enable && !config.services.gitlab.enable && !config.services.gitolite.enable && !config.services.gerrit.enable)
-      (lib.mkForce [ "/etc/ssh/authorized_keys.d/%u" ]);
+    authorizedKeysFiles =
+      lib.mkIf (!config.services.gitea.enable && !config.services.gitlab.enable && !config.services.gitolite.enable && !config.services.gerrit.enable)
+      (lib.mkForce ["/etc/ssh/authorized_keys.d/%u"]);
 
     # unbind gnupg sockets if they exists
     extraConfig = "StreamLocalBindUnlink yes";
@@ -125,14 +130,21 @@
     password = "'sha1:1b961dc713fb:88483270a63e57d18d43cf337e629539de1436ba'";
     port = 9999;
   };
-  
+
   services.jupyterhub.enable = true;
-  services.ollama.acceleration = "rocm";
+  services.ollama = {
+    enable = true;
+    acceleration = "rocm";
+    environmentVariables = {
+      HCC_AMDGPU_TARGET = "gfx900";
+    };
+    rocmOverrideGfx = "9.0.0";
+  };
 
-#  services.tabby.enable = true;
-#  services.tabby.acceleration = "rocm";
+  #  services.tabby.enable = true;
+  #  services.tabby.acceleration = "rocm";
 
-#  services.clickhouse.enable = true;
+  #  services.clickhouse.enable = true;
   services.usbmuxd.enable = true;
   services.pcscd.enable = true;
 }
