2b74166741508d9e2a7badc870ba34dbfe0626ca
Darrell Kundel <darrell@demiflat.com>
2024-07-15 16:18:56 -0700

    ollama

--- services.nix
+++ services.nix
@@ -135,10 +135,10 @@
   services.ollama = {
     enable = true;
     acceleration = "rocm";
-    environmentVariables = {
-      HCC_AMDGPU_TARGET = "gfx900";
-      HSA_OVERRIDE_GFX_VERSION = "9.0.0";
-    };
+    #    environmentVariables = {
+    #      HCC_AMDGPU_TARGET = "gfx900";
+    #      HSA_OVERRIDE_GFX_VERSION = "9.0.0";
+    #    };
   };
 
   #  services.tabby.enable = true;
