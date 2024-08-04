6b9f165d076173b489ab0716a054831e0c07e52d
Darrell Kundel <darrell@demiflat.com>
2024-08-03 23:26:25 -0700

    +ollama

--- services.nix
+++ services.nix
@@ -132,14 +132,14 @@
   #};
 
   #  services.jupyterhub.enable = true;
-  #  services.ollama = {
-  #    enable = true;
-  #    acceleration = "rocm";
-  #    #    environmentVariables = {
-  #    #      HCC_AMDGPU_TARGET = "gfx900";
-  #    #      HSA_OVERRIDE_GFX_VERSION = "9.0.0";
-  #    #    };
-  #  };
+  services.ollama = {
+    enable = true;
+    acceleration = "rocm";
+    environmentVariables = {
+      HCC_AMDGPU_TARGET = "gfx900";
+      HSA_OVERRIDE_GFX_VERSION = "9.0.0";
+    };
+  };
 
   #  services.tabby.enable = true;
   #  services.tabby.acceleration = "rocm";
