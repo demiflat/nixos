8574ec193b747f9561f223b3df39e97dec8eae20
Darrell Kundel <darrell@demiflat.com>
2024-09-17 20:22:05 -0700

    net.fix

--- network.nix
+++ network.nix
@@ -225,7 +225,7 @@
     # The notion of "online" is a broken concept
     # https://github.com/systemd/systemd/blob/e1b45a756f71deac8c1aa9a008bd0dab47f64777/NEWS#L1
     services.NetworkManager-wait-online.enable = false;
-    wait-online.enable = false;
+    network.wait-online.enable = false;
     # Do not take down the network for too long when upgrading,
     # This also prevents failures of services that are restarted instead of stopped.
     # It will use `systemctl restart` rather than stopping it with `systemctl stop`
