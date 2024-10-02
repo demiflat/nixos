20d815572272007284fd3311b36a4c3d6093c2be
Darrell Kundel <darrell@demiflat.com>
2024-10-02 10:24:27 -0700

    portals

--- configuration.nix
+++ configuration.nix
@@ -103,8 +103,8 @@
     #    #wlr.enable = true;
     xdgOpenUsePortal = true;
     extraPortals = [
-      #pkgs.xdg-desktop-portal-hyprland
-      #pkgs.xdg-desktop-portal-gtk
+      pkgs.xdg-desktop-portal-hyprland
+      pkgs.xdg-desktop-portal-gtk
       pkgs.xdg-desktop-portal-gnome
     ];
   };
