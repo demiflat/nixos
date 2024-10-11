e5ab24a71b44171af94f19209edb26429ae09f85
Darrell Kundel <darrell@demiflat.com>
2024-10-10 19:22:52 -0700

    nixvim

--- flake.nix
+++ flake.nix
@@ -4,8 +4,8 @@
     #nixos-unstable-small.url = "github:NixOS/nixpkgs/nixos-unstable-small";
     #nixvim.url = "github:nix-community/nixvim";
     #nixvim.inputs.nixpkgs.follows = "nixpkgs";
-    # nixvim.url = "github:mikaelfangel/nixvim-config";
-    nixvim.url = "/development/os/vi/dakvim";
+    nixvim.url = "github:mikaelfangel/nixvim-config";
+    # nixvim.url = "/development/os/vi/dakvim";
     # jeezyvim.url = "github:LGUG2Z/JeezyVim";
     #home-manager.url = "github:nix-community/home-manager";
     #home-manager.inputs.nixpkgs.follows = "nixpkgs";
