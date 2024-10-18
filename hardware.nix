df9f5f252ab674847b31ce087f7a9e7d2fa746d6
Darrell Kundel <darrell@demiflat.com>
2024-10-18 00:30:40 -0700

    -amd

--- hardware.nix
+++ hardware.nix
@@ -4,21 +4,40 @@
   pkgs,
   modulesPath,
   ...
-}: {
+}:
+{
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
   boot.initrd.systemd.enable = true;
-  boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod"];
-  boot.initrd.kernelModules = ["kvm-amd" "amdgpu" "i2c-dev"];
-  boot.kernelModules = ["kvm-amd" "amdgpu" "i2c-dev"];
-  boot.extraModulePackages = [];
+  boot.initrd.availableKernelModules = [
+    "xhci_pci"
+    "ahci"
+    "nvme"
+    "usbhid"
+    "usb_storage"
+    "sd_mod"
+  ];
+  boot.initrd.kernelModules = [
+    "kvm-amd"
+    "amdgpu"
+    "i2c-dev"
+  ];
+  boot.kernelModules = [
+    "kvm-amd"
+    "amdgpu"
+    "i2c-dev"
+  ];
+  boot.extraModulePackages = [ ];
 
   fileSystems."/" = {
     device = "/dev/disk/by-label/nixos";
     fsType = "btrfs";
-    options = ["subvol=@" "compress=zstd"];
+    options = [
+      "subvol=@"
+      "compress=zstd"
+    ];
   };
 
   # fileSystems."/nix" =
@@ -40,16 +59,16 @@
   fileSystems."/home" = {
     device = "/dev/disk/by-label/home";
     fsType = "btrfs";
-    options = ["subvol=@"];
+    options = [ "subvol=@" ];
   };
 
   fileSystems."/development" = {
     device = "/dev/disk/by-label/development";
     fsType = "btrfs";
-    options = ["subvol=@"];
+    options = [ "subvol=@" ];
   };
 
-  swapDevices = [];
+  swapDevices = [ ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
@@ -62,12 +81,12 @@
     cpu.amd.updateMicrocode = true;
     enableRedistributableFirmware = true;
     graphics.enable = true;
-    graphics.extraPackages = with pkgs; [
-      amdvlk
-      rocmPackages.clr
-      rocm-opencl-icd
-      rocm-opencl-runtime
-    ];
+    #    graphics.extraPackages = with pkgs; [
+    #amdvlk
+    #rocmPackages.clr
+    #rocm-opencl-icd
+    #rocm-opencl-runtime
+    #    ];
     keyboard.qmk.enable = true;
     sane.enable = true;
     pulseaudio.enable = false;
