63e52f8c87439048ff3e4305606ee84cb0529758
Darrell Kundel <darrell@demiflat.com>
2024-10-22 22:50:45 -0700

    tweaks

--- environment.nix
+++ environment.nix
@@ -10,20 +10,20 @@
       gtk-application-prefer-dark-theme = 1
       gtk-error-bell = false
       gtk-theme-name = "Adwaita-dark"
-      gtk-icon-theme-name="Adwaita"
-      gtk-font-name="Cantarell 11"
-      gtk-cursor-theme-name="dmz-black"
-      gtk-cursor-theme-size=24
-      gtk-toolbar-style=GTK_TOOLBAR_ICONS
-      gtk-toolbar-icon-size=GTK_ICON_SIZE_LARGE_TOOLBAR
-      gtk-button-images=0
-      gtk-menu-images=0
       gtk-enable-event-sounds=0
       gtk-enable-input-feedback-sounds=0
       gtk-xft-antialias=1
       gtk-xft-hinting=1
       gtk-xft-hintstyle="hintfull"
       gtk-xft-rgba="rgb"
+      gtk-icon-theme-name="Adwaita"
+      gtk-font-name="Cantarell 11"
+      gtk-cursor-theme-name="Vanilla-DMZ-AA"
+      gtk-cursor-theme-size=24
+      gtk-toolbar-style=GTK_TOOLBAR_ICONS
+      gtk-toolbar-icon-size=GTK_ICON_SIZE_SMALL_TOOLBAR
+      gtk-button-images=1
+      gtk-menu-images=1
     '';
     "xdg/gtk-3.0/settings.ini".text = ''
       [Settings]
