fc88bd528d0d8428a968f350aa4b015f3a1e01ac
Darrell Kundel <darrell@demiflat.com>
2024-11-09 16:38:03 -0800

    nixvim

--- flake.nix
+++ flake.nix
@@ -5,37 +5,43 @@
     #nixvim.url = "github:nix-community/nixvim";
     #nixvim.inputs.nixpkgs.follows = "nixpkgs";
     #nixvim.url = "github:mikaelfangel/nixvim-config";
-    nixvim.url = "github:elythh/nixvim";
+    #nixvim.url = "github:elythh/nixvim";
+    nixvim.url = "github:dc-tec/nixvim";
     # nixvim.url = "/development/os/vi/dakvim";
     # jeezyvim.url = "github:LGUG2Z/JeezyVim";
     #home-manager.url = "github:nix-community/home-manager";
     #home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = inputs: let
-    system = "x86_64-linux";
-    pkgs = import inputs.nixpkgs {
-      inherit system;
-      overlays = [
-        (final: prev: {
-          neovim = inputs.nixvim.packages.${pkgs.system}.default;
-        })
-      ];
-      config = {
-        permittedInsecurePackages = [];
-        allowUnfree = true;
-      };
-    };
-  in {
-    # your configuration
-    nixosConfigurations = {
-      yoshi = inputs.nixpkgs.lib.nixosSystem {
+  outputs =
+    inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import inputs.nixpkgs {
         inherit system;
-        specialArgs = {inherit inputs pkgs;};
-        modules = [
-          ./configuration.nix
-          #inputs.nixvim.nixosModules.nixvim
+        overlays = [
+          (final: prev: {
+            neovim = inputs.nixvim.packages.${pkgs.system}.default;
+          })
         ];
+        config = {
+          permittedInsecurePackages = [ ];
+          allowUnfree = true;
+        };
+      };
+    in
+    {
+      # your configuration
+      nixosConfigurations = {
+        yoshi = inputs.nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = {
+            inherit inputs pkgs;
+          };
+          modules = [
+            ./configuration.nix
+            #inputs.nixvim.nixosModules.nixvim
+          ];
+        };
       };
     };
-  };
 }
