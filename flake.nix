9823b9281f71d2e479b48b5aaeb30ba847608f1c
Darrell Kundel <darrell@demiflat.com>
2025-04-09 18:58:56 -0700

    +lix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,10 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+     lix-module = {
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.92.0-3.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };   
 #    zen-browser.url = "github:0xc000022070/zen-browser-flake";
     #zen-browser.url = "github:MarceColl/zen-browser-flake";
     #isd.url = "https://github.com/isd-project/isd";
@@ -16,13 +20,14 @@
     #home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs =
-    { self, nixpkgs, ... }@inputs:
+    { self, nixpkgs, lix-module, ... }@inputs:
     {
       nixosConfigurations = {
         yoshi = nixpkgs.lib.nixosSystem {
 #          system = "x86_64-linux";
           specialArgs = { inherit inputs; system = "x86_64-linux"; };
           modules = [
+            lix-module.nixosModules.default
             #inputs.isd.default
             ./configuration.nix
           ];
