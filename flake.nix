71916f60fb391bbd692b7616d3776e701d475687
Darrell Kundel <darrell@demiflat.com>
2025-08-18 22:03:32 -0700

    +lix_HEAD

--- flake.nix
+++ flake.nix
@@ -1,10 +1,21 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.3-1.tar.gz";
+    lix = {
+      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
+      flake = false;
+    };
+
+    lix-module = {
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
-    };   
+      inputs.lix.follows = "lix";
+    };
+
+    #    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    # lix-module = {
+    #  url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.3-1.tar.gz";
+    #  inputs.nixpkgs.follows = "nixpkgs";
+    #};   
 #    zen-browser.url = "github:0xc000022070/zen-browser-flake";
     #zen-browser.url = "github:MarceColl/zen-browser-flake";
     #isd.url = "https://github.com/isd-project/isd";
