9fa9f0f9c2e17f8d4b619ec08984aac0e3c6b31a
Darrell Kundel <darrell@demiflat.com>
2026-01-09 11:04:03 -0800

    nixpkgs.url

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
 {
   inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     lix = {
       url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
       flake = false;
