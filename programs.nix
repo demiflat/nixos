42c6abf8010e4df36f7f9b16055cae56be512f72
Darrell Kundel <darrell@demiflat.com>
2026-01-13 20:45:47 -0800

    +wallpaper.path

--- programs.nix
+++ programs.nix
@@ -16,7 +16,7 @@
       cursorTheme.name = "nordzy";
       settings = {
         background = {
-         path = pkgs.nixos-artwork.wallpapers.stripes;
+         path = ${pkgs.nixos-artwork.wallpapers.stripes}/share/backgrounds/nixos/nix-wallpaper-stripes.png;
          fit = "Cover";
         };
       };
