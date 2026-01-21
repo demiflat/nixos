da0d4714204ae996cc2c68a34b6e7ad240f3d9b6
Darrell Kundel <darrell@demiflat.com>
2026-01-21 11:41:47 -0800

    +xdg-desktop-portal-hyprland

--- programs.nix
+++ programs.nix
@@ -18,7 +18,8 @@
       settings = {
         background = {
           #path = "${pkgs.nixos-artwork.wallpapers.stripes}/share/backgrounds/nixos/nix-wallpaper-stripes.png";
-          path = "${pkgs.nixos-artwork.wallpapers.stripes.gnomeFilePath} }";
+          #path = "${pkgs.nixos-artwork.wallpapers.stripes.gnomeFilePath} }";
+          path = "/nix/store/9dv4n8qajmbjd7k1yxrnw6q6hs7pxzzy-nix-wallpaper-stripes.png";
           fit = "Fill";
         };
       };
@@ -72,6 +73,7 @@
       enable = true;
       withUWSM = true;
       xwayland.enable = true;
+      portalPackage = pkgs.xdg-desktop-portal-hyprland;
     };
     hyprlock.enable = true;
     waybar.enable = true;
