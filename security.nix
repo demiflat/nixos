56ee82207d952f8acff84f879715b7bec4c45974
Darrell Kundel <darrell@demiflat.com>
2026-02-02 10:59:31 -0800

    +cups.security

--- security.nix
+++ security.nix
@@ -16,4 +16,32 @@
   # unlock GPG keyring on login
   security.pam.services.greetd.gnupg.enable = true;
   security.pam.services.greetd.enableGnomeKeyring = true;
+
+  systemd.services."cups".serviceConfig = {
+    ProtectClock = true;
+    ProtectKernelTunables = true;
+    ProtectKernelModules = true;
+    ProtectKernelLogs = true;
+    SystemCallFilter = "~@clock @cpu-emulation @debug @obsolete @module @mount @raw-io @reboot @swap";
+    ProtectControlGroups = true;
+    RestrictNamespaces = true;
+    LockPersonality = true;
+    MemoryDenyWriteExecute = true;
+    RestrictRealtime = true;
+    RestrictSUIDSGID = true;
+  };
+
+  systemd.services."cups-browsed".serviceConfig = {
+    ProtectClock = true;
+    ProtectKernelTunables = true;
+    ProtectKernelModules = true;
+    ProtectKernelLogs = true;
+    SystemCallFilter = "~@clock @cpu-emulation @debug @obsolete @module @mount @raw-io @reboot @swap";
+    ProtectControlGroups = true;
+    RestrictNamespaces = true;
+    LockPersonality = true;
+    MemoryDenyWriteExecute = true;
+    RestrictRealtime = true;
+    RestrictSUIDSGID = true;
+  };
 }
