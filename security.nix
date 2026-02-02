b9ddaabc87114b6d42b2a106d0d3806d13ff9e2b
Darrell Kundel <darrell@demiflat.com>
2026-02-02 11:15:09 -0800

    security.cups

--- security.nix
+++ security.nix
@@ -23,8 +23,7 @@
     ##############
     RestrictAddressFamilies = "AF_INET AF_INET6 AF_NETLINK AF_UNIX";
     IPAddressDeny = "any";
-    IPAddressAllow = "localhost";
-    IPAddressAllow = "10.1.1.0/8";
+    IPAddressAllow = [ "localhost" "10.1.1.0/8" ];
 
     ###############
     # File system #
