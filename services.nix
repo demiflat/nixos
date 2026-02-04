ed135fe68bec4252e985843a33a148415f8cc6c1
Darrell Kundel <darrell@demiflat.com>
2026-02-04 09:57:00 -0800

    +cups.browsed

--- services.nix
+++ services.nix
@@ -104,7 +104,7 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
-  services.printing.browsed.enable = false;
+  services.printing.browsed.enable = true;
   services.printing.drivers = [
     #  "cups-brother-dcpt725dw"
     #  "cups-brother-hl1110"
