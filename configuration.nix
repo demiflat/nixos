f33a3d3b5ac7d9300ee30d5b78d8fc25947e1af8
Darrell Kundel <darrell@demiflat.com>
2026-02-22 11:18:50 -0800

    -kanata

--- configuration.nix
+++ configuration.nix
@@ -21,6 +21,7 @@
     ./boot.nix
     ./greetd.nix
     ./kb.nix
+    #./kanata.nix
     ./cpufreq.nix
     ./ddc.nix
     ./locale.nix
--- kb.nix
+++ kb.nix
@@ -20,48 +20,4 @@
   # Ensure the uinput group exists
   users.groups.uinput = { };
 
-  # Add the Kanata service user to necessary groups
-  systemd.services.kanata-internalKeyboard.serviceConfig = {
-    SupplementaryGroups = [
-      "input"
-      "uinput"
-    ];
-  };
-
-  services.kanata = {
-    enable = true;
-    keyboards = {
-      internalKeyboard = {
-        devices = [
-          # Replace the paths below with the appropriate device paths for your setup.
-          # Use `ls /dev/input/by-path/` to find your keyboard devices.
-          "/dev/input/by-id/usb-nuphy_NuPhy_Field75_HE_2023-11-15-event-kbd"
-          "/dev/input/by-id/usb-CATEX_TECH._87EC-S_CA2018080005-event-kbd"
-        ];
-        extraDefCfg = "process-unmapped-keys yes
-                       log-layer-changes yes";
-        config = ''
-          (defsrc
-           caps
-           h j k l
-          )
-          (defvar
-           tap-time 200
-           hold-time 200
-          )
-          (defalias
-           caps (layer-while-held $vim)
-          )
-          (deflayer default
-           @caps
-           h j k l
-          )
-          (deflayer $vim
-           _
-           left down up right
-          )
-        '';
-      };
-    };
-  };
 }
