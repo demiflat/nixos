f7d81e42ef3fc9bcc7b9b3283edef18dcdd629f0
Darrell Kundel <darrell@demiflat.com>
2026-02-24 22:21:53 -0800

    -vllm

--- packages.nix
+++ packages.nix
@@ -11,7 +11,7 @@
   environment.systemPackages = with pkgs; [
     # llm
     llama-cpp
-    vllm
+    #vllm
     #    inputs.zen-browser.packages."${system}".twilight
     abcde
     akkuPackages.scheme-langserver
