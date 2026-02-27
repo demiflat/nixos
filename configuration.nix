7b86210c48fe1560baab12305e66f5b4c1cbebd5
Darrell Kundel <darrell@demiflat.com>
2026-02-26 23:41:45 -0800

    font

--- configuration.nix
+++ configuration.nix
@@ -108,7 +108,7 @@
     #package = pkgs.nixFlakes;
   };
 
-  #console.font = "gr737b-9x16-medieval";
+  #console.font = "gr737b-9x16-medieval.psfu.gz";
   console.font = "${pkgs.kbd}/share/consolefonts/gr737b-9x16-medieval.psfu.gz";
 
   # For the hacking.
