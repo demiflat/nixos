71dedbc7a057ab8606c7f1115e867601d9ceb715
Darrell Kundel <darrell@demiflat.com>
2026-03-20 21:00:02 -0700

    -vllm

--- packages.nix
+++ packages.nix
@@ -576,7 +576,7 @@
     visidata
     vit
     vivid
-    vllm
+    #vllm
     #      vscodeExtensions = with vscode-extensions; [
     #      vscode = vscodium;
     #    (vscode-with-extensions.override {
