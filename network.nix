6d403ffd84b27a2000e7a6ebff9794f49f265147
Darrell Kundel <darrell@demiflat.com>
2026-03-23 16:56:19 -0700

    +metric

--- network.nix
+++ network.nix
@@ -187,6 +187,7 @@
           dhcpV4Config.UseRoutes = "no";
           dhcpV4Config.UseDNS = "no";
           linkConfig.RequiredForOnline = "no";
+          dhcpV4Config.Metric = 2048;
           dhcpV4Config.RouteMetric = 2048;
           # dhcpV6Config.RouteMetric = 2048;
         };
