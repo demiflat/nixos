715bc44ef1d5109a52b8f474746dda17d5ce7f1b
Darrell Kundel <darrell@demiflat.com>
2026-03-23 16:54:49 -0700

    routemetric

--- network.nix
+++ network.nix
@@ -187,7 +187,7 @@
           dhcpV4Config.UseRoutes = "no";
           dhcpV4Config.UseDNS = "no";
           linkConfig.RequiredForOnline = "no";
-          # dhcpV4Config.RouteMetric = 2048;
+          dhcpV4Config.RouteMetric = 2048;
           # dhcpV6Config.RouteMetric = 2048;
         };
         # "41-sonic" = {
