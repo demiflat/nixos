8d335348ff4c33bf9409269f4dae5f47346b50ae
Darrell Kundel <darrell@demiflat.com>
2026-04-03 15:32:32 -0700

    llama-cpp.override

--- configuration.nix
+++ configuration.nix
@@ -262,10 +262,58 @@
   # security.tpm2.tctiEnvironment.enable = true;
 
   # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-  nixpkgs.hostPlatform.system = "x86_64-linux";
-  nixpkgs.config.cudaSupport = true;
-  nixpkgs.config.input-fonts.acceptLicense = true;
+  nixpkgs = {
+    hostPlatform.system = "x86_64-linux";
+    config = {
+      allowUnfree = true;
+      cudaSupport = true;
+      input-fonts.acceptLicense = true;
+      packageOverrides = pkgs: {
+
+        # Override llama-cpp to latest version b6150 with CUDA support
+        llama-cpp =
+          (pkgs.llama-cpp.override {
+            cudaSupport = true;
+            rocmSupport = false;
+            metalSupport = false;
+            # Enable BLAS for optimized CPU layer performance (OpenBLAS)
+            # This is crucial for models using split-mode or CPU offloading
+            blasSupport = true;
+          }).overrideAttrs
+            (oldAttrs: rec {
+              version = "8204";
+              src = pkgs.fetchFromGitHub {
+                owner = "ggml-org";
+                repo = "llama.cpp";
+                tag = "b${version}";
+                hash = "sha256-j3RLNiY6u36qdLah4Zcrac804Ub1wnBtv066PtzBvt0=";
+                leaveDotGit = true;
+                postFetch = ''
+                  git -C "$out" rev-parse --short HEAD > $out/COMMIT
+                  find "$out" -name .git -print0 | xargs -0 rm -rf
+                '';
+              };
+              npmDepsHash = "sha256-FKjoZTKm0ddoVdpxzYrRUmTiuafEfbKc4UD2fz2fb8A=";
+              # Enable native CPU optimizations for massively better CPU performance
+              # This enables AVX, AVX2, AVX-512, FMA, etc. for your specific CPU
+              # NOTE: This is intentionally opposite of nixpkgs (which uses -DGGML_NATIVE=off
+              # for reproducible builds). We sacrifice portability for faster CPU layers.
+              cmakeFlags = (oldAttrs.cmakeFlags or [ ]) ++ [
+                "-DGGML_NATIVE=ON"
+                "-DCMAKE_CUDA_ARCHITECTURES=120" # RTX 5060ti - needed since sandbox has no GPU
+              ];
+
+              # Disable Nix's NIX_ENFORCE_NO_NATIVE which strips -march=native flags
+              # See: https://github.com/NixOS/nixpkgs/issues/357736
+              # See: https://github.com/NixOS/nixpkgs/pull/377484 (intentionally contradicts this)
+              preConfigure = ''
+                export NIX_ENFORCE_NO_NATIVE=0
+                ${oldAttrs.preConfigure or ""}
+              '';
+            });
+      };
+    };
+  };
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
