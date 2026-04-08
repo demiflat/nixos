3eca68ad604e0b806000dbb560ec2c66d4fc42ee
Darrell Kundel <darrell@demiflat.com>
2026-04-07 21:57:57 -0700

    -flox

--- flake.nix
+++ flake.nix
@@ -10,10 +10,10 @@
       url = "github:nix-community/NUR";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    flox = {
-      url = "github:flox/flox";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    #flox = {
+    #  url = "github:flox/flox";
+    #  inputs.nixpkgs.follows = "nixpkgs";
+    #};
     flake-utils.url = "github:numtide/flake-utils";
     #llama-cpp = {
     #  url = "github:ggerganov/llama.cpp";
@@ -61,7 +61,7 @@
       nvf,
       nur,
       nix-index-database,
-      flox,
+    #flox,
       #llama-cpp,
       ...
     }@inputs:
@@ -90,7 +90,7 @@
             #inputs.isd.default
             #lix-module.nixosModules.default
             nvf.nixosModules.default
-            flox.nixosModules.flox
+            #flox.nixosModules.flox
             nix-index-database.nixosModules.default
             { programs.nix-index-database.comma.enable = true; }
             nur.modules.nixos.default
