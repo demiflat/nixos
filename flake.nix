4c7c71c99160fb32fa308eee61117de48777befb
Darrell Kundel <darrell@demiflat.com>
2026-07-24 08:10:45 -0700

    26.05

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    #nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+    #nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-26.05";
     nvf.url = "github:notashelf/nvf";
     #opencode.url = "github:anomalyco/opencode";
