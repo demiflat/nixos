d9054d3d5502d7912d1de96236de25ee9b450b9c
Darrell Kundel <darrell@demiflat.com>
2026-08-08 21:57:58 -0700

    _fixed_substituters

--- configuration.nix
+++ configuration.nix
@@ -98,6 +98,7 @@
       substituters = [
         "https://cache.nixos.org"
         "https://cache.nixos-cuda.org"
+        "https://nix-community.cachix.org"
         "https://hyprland.cachix.org"
         "https://cache.flox.dev"
         "https://ros.cachix.org"
@@ -105,6 +106,7 @@
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
         "ros.cachix.org-1:dSyZxI8geDCJrwgvCOHDoAfOm5sV1wCPjBkKL+38Rvo="
