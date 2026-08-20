1b4df8949c37b68085ee5abcb9beee8633d52178
Darrell Kundel <darrell@demiflat.com>
2026-08-19 23:24:15 -0700

    nvidia.production

--- hardware.nix
+++ hardware.nix
@@ -107,7 +107,8 @@
     nvidia = {
       branch = "latest";
       #package = config.boot.kernelPackages.nvidiaPackages.latest;
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      #package = config.boot.kernelPackages.nvidiaPackages.stable;
+      package = config.boot.kernelPackages.nvidiaPackages.production;
       #  enabled = true;
       #open = false;
       open = true;
