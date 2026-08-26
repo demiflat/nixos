c0d231bf92e9d7f37ffc9b7b3dc5383ff42ed0df
Darrell Kundel <darrell@demiflat.com>
2026-08-25 22:09:09 -0700

    themes

--- environment.nix
+++ environment.nix
@@ -10,14 +10,14 @@
     "xdg/gtk-2.0/gtkrc".text = ''
       gtk-application-prefer-dark-theme = 1
       gtk-error-bell = false
-      gtk-theme-name = Catppuccin-GTK
+      gtk-theme-name = Dexy-GTK
       gtk-enable-event-sounds=0
       gtk-enable-input-feedback-sounds=0
       gtk-xft-antialias=1
       gtk-xft-hinting=1
       gtk-xft-hintstyle="hintfull"
       gtk-xft-rgba="rgb"
-      gtk-icon-theme-name="Adwaita"
+      gtk-icon-theme-name="Nordzy-catppuccin-latte-peach"
       gtk-font-name="NotoSans Nerd Font Propo"
       gtk-cursor-theme-name=dmz-black
       gtk-cursor-theme-size=24
@@ -31,7 +31,7 @@
       gtk-error-bell = false
       gtk-application-prefer-dark-theme=1
       gtk-font-name="NotoSans Nerd Font Propo"
-      gtk-theme-name = Catppuccin-GTK
+      gtk-theme-name = Dexy-GTK
       gtk-cursor-theme-name=dmz-black
       gtk-cursor-theme-size=24
     '';
@@ -40,7 +40,7 @@
       gtk-error-bell = false
       gtk-application-prefer-dark-theme = 1
       gtk-font-name="NotoSans Nerd Font Propo"
-      gtk-theme-name = Catppuccin-GTK
+      gtk-theme-name = Dexy-GTK
       gtk-cursor-theme-name=dmz-black
       gtk-cursor-theme-size=24
     '';
--- programs.nix
+++ programs.nix
@@ -72,10 +72,12 @@
       {
         settings."org/gnome/desktop/interface" = {
           #gtk-theme = "Adwaita-dark";
-          gtk-theme = "Mojave-Dark";
+          #gtk-theme = "Mojave-Dark";
+          gtk-theme = "Dexy-GTK";
           #icon-theme = "Flat-Remix-Red-Dark";
           #icon-theme = "Adwaita-dark";
-          icon-theme = "Canta";
+          #icon-theme = "Canta";
+          icon-theme = "Nordzy-catppuccin-latte-peach";
           font-name = "Noto Sans Medium 11";
           document-font-name = "Noto Sans Medium 11";
           monospace-font-name = "Noto Sans Mono Medium 11";
