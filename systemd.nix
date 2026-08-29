07443df6481e007d59433d8b9fd17fbd742e381a
Darrell Kundel <darrell@demiflat.com>
2026-08-29 11:21:37 -0700

    +systemd.services.ensure-printers.after

--- systemd.nix
+++ systemd.nix
@@ -1,4 +1,6 @@
 { lib, ... }:
 {
   systemd.user.services.waybar.wantedBy = lib.mkForce [ ];
+  systemd.services.ensure-printers.after = [ "network-online.target" ];
+  systemd.services.ensure-printers.wants = [ "network-online.target" ];
 }
